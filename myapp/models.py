7520fbf63983213dbf75ac3ea50932519b7b6208
rudhra <rudhra@panda.local>
2023-12-07 10:42:21 +0530

    added new changes

--- myapp/models.py
+++ myapp/models.py
@@ -6,9 +6,8 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.CharField(max_length=200, null=True, blank=True)
+    # image = models.CharField(max_length=200, null=True, blank=True)
     created_at = models.DateTimeField("Created at", auto_now_add=True)
-    # Add other fields as needed
 
     def __str__(self):
         return self.name
@@ -18,7 +17,6 @@ class ProductInventory(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     inventory_count = models.PositiveIntegerField(default=100)
     created_at = models.DateTimeField("Created at", auto_now_add=True)
-
 
     def __str__(self):
         return f"{self.product.name} - Inventory: {self.inventory_count}"
