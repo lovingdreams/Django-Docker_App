5ed4bab0030a0ffe4dc0864a6b8ec485636fd6cc
Lovingdreams <124341333+lovingdreams@users.noreply.github.com>
2023-12-21 14:37:19 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
+    path("prometheus-worke/", include("django_prometheus.urls"))
 ]
